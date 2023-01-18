18206be08e8b2fc3d687e86f6c92f218c22a7e28
marcos ventura <marcosventura.dev@gmail.com>
2023-01-18 20:21:01 -0300

    resolução de questão em java

---
+++ src/main/java/id/sistemavendaspa/aulasudemyjs/AulasUdemyJs.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package id.sistemavendaspa.aulasudemyjs;
+
+/**
+ *
+ * @author joaom
+ */
+public class AulasUdemyJs {
+
+    public static void main(String[] args) {
+     
+       for (int i = 0; i<= 100; i++){
+           if(i%2 != 0){
+              System.out.println(i);
+           }
+       }
+    }
+}
